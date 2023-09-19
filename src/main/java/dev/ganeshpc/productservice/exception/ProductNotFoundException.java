e8c9eec880c39d5f69b5abac8997b56c92b0d6ca
ganeshpc <gnesh.pc@gmail.com>
2023-09-19 20:57:26 +0530

    New way to handle exception Add ResponseStatus annotation directly to the exception class

--- src/main/java/dev/ganeshpc/productservice/exception/ProductNotFoundException.java
+++ src/main/java/dev/ganeshpc/productservice/exception/ProductNotFoundException.java
@@ -1,5 +1,9 @@
 package dev.ganeshpc.productservice.exception;
 
+// import org.springframework.http.HttpStatus;
+// import org.springframework.web.bind.annotation.ResponseStatus;
+
+// @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends Exception {
     
     public ProductNotFoundException(String message) {
--- src/main/java/dev/ganeshpc/productservice/models/Product.java
+++ src/main/java/dev/ganeshpc/productservice/models/Product.java
@@ -1,8 +1,12 @@
 package dev.ganeshpc.productservice.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
+@Getter
+@Setter
 public class Product extends BaseModel {
     private String title;
     private String description;
