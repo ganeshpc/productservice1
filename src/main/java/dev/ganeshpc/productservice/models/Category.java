95ce50bfc39aa15e5a7694a00ddf36534903279c
ganeshpc <gnesh.pc@gmail.com>
2023-10-08 14:10:13 +0530

    Added database and the class annotations

--- src/main/java/dev/ganeshpc/productservice/models/Category.java
+++ src/main/java/dev/ganeshpc/productservice/models/Category.java
@@ -1,7 +1,9 @@
 package dev.ganeshpc.productservice.models;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 
+@Entity
 @AllArgsConstructor
 public class Category extends BaseModel {
     private String title;
--- src/main/java/dev/ganeshpc/productservice/models/Product.java
+++ src/main/java/dev/ganeshpc/productservice/models/Product.java
@@ -1,9 +1,12 @@
 package dev.ganeshpc.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @AllArgsConstructor
 @Getter
 @Setter
@@ -11,6 +14,7 @@ public class Product extends BaseModel {
     private String title;
     private String description;
     private String image;
+    @ManyToOne
     private Category category;
     private double price;
 }
